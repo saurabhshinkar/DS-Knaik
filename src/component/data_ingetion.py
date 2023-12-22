cfd803fbbd97bb9bebcf79c268e120f63c0168bf
Saurabh Shinkar <saurabh@leobrix.com>
2023-12-22 17:23:57 +0530

    model evaluation

--- src/component/data_ingetion.py
+++ src/component/data_ingetion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.component.data_transformation import DataTransformation
 from src.component.data_transformation import DataTransformationConfig
 
-# from src.component.model_trainer import ModelTrainerConfig
-# from src.component.model_trainer import ModelTrainer
+from src.component.model_trainer import ModelTrainerConfig
+from src.component.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -55,3 +55,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
