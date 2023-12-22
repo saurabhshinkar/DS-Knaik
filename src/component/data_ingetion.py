940643369f77659b2eb8643535e7a6bd1e477320
Saurabh Shinkar <saurabh@leobrix.com>
2023-12-22 14:49:50 +0530

    data transformation done

--- src/component/data_ingetion.py
+++ src/component/data_ingetion.py
@@ -7,8 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.component.data_transformation import DataTransformation
-# from src.component.data_transformation import DataTransformationConfig
+from src.component.data_transformation import DataTransformation
+from src.component.data_transformation import DataTransformationConfig
 
 # from src.component.model_trainer import ModelTrainerConfig
 # from src.component.model_trainer import ModelTrainer
@@ -52,3 +52,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
